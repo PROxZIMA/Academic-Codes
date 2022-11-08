16ec684598a068021eb5bf555abdf1b45226661f
PROxZIMA <pratikbpingale9075@gmail.com>
2022-11-08 09:09:45 +0530

    Refactor: DAA Mini Project

--- Semester 7/LP3/DAA/A8/A8.py
+++ Semester 7/LP3/DAA/A8/A8.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import argparse
 import random
-import sys
 from contextlib import contextmanager
-from multiprocessing import Manager, Pool
+from multiprocessing import Pool, cpu_count
 from timeit import default_timer as time
+
+from tabulate import tabulate
+
+CPU_COUNT = cpu_count()
 
 
 class Timer:
@@ -31,16 +38,6 @@ class Timer:
         self._time_per_step[step] += time()
 
 
-def merge_sort_multiple(results, array):
-    """Async parallel merge sort."""
-    results.append(merge_sort(array))
-
-
-def merge_multiple(results, array_part_left, array_part_right):
-    """Merge two sorted lists in parallel."""
-    results.append(merge(array_part_left, array_part_right))
-
-
 def merge_sort(array):
     """Perform merge sort."""
     array_length = len(array)
@@ -49,15 +46,17 @@ def merge_sort(array):
         return array
 
     middle_index = array_length // 2
-    left = array[:middle_index]
-    right = array[middle_index:]
-    left = merge_sort(left)
-    right = merge_sort(right)
+    left = merge_sort(array[:middle_index])
+    right = merge_sort(array[middle_index:])
     return merge(left, right)
 
 
-def merge(left, right):
+def merge(*arrays):
     """Merge two sorted lists."""
+
+    # Support explicit left/right args, as well as a two-item
+    # tuple which works more cleanly with multiprocessing.
+    left, right = arrays[0] if len(arrays) == 1 else arrays
     sorted_list = [0] * (len(left) + len(right))
     i = j = k = 0
 
@@ -99,115 +98,255 @@ def parallel_merge_sort(array, ps_count):
     timer.start_for("sort")
 
     # Divide the list in chunks
-    step = int(length / ps_count)
+    step = int(len(array) / ps_count)
 
-    # Instantiate a multiprocessing.Manager object to store the output of each process.
-    manager = Manager()
-    results = manager.list()
-
+    # Creates a pool of worker processes, one per CPU core.
+    # We then split the initial data into partitions, sized equally per
+    # worker, and perform a regular merge sort across each partition.
     with process_pool(size=ps_count) as pool:
-        for i in range(ps_count):
-            # We create a new Process object and assign the
-            # merge_sort_multiple function to it,
-            # using as input a sublist
-            if i < ps_count - 1:
-                chunk = array[i * step : (i + 1) * step]
-            else:
-                # Get the remaining elements in the list
-                chunk = array[i * step :]
-            pool.apply_async(merge_sort_multiple, (results, chunk))
+        array = [array[i * step : (i + 1) * step] for i in range(ps_count)] + [array[ps_count * step :]]
+        array = pool.map(merge_sort, array)
 
-    timer.stop_for("sort")
+        timer.stop_for("sort")
+        timer.start_for("merge")
 
-    print("Performing final merge.")
+        # We can use multiprocessing again to merge sub-lists in parallel.
+        while len(array) > 1:
+            # If the number of partitions remaining is odd, we pop off the
+            # last one and append it back after one iteration of this loop,
+            # since we're only interested in pairs of partitions to merge.
+            extra = array.pop() if len(array) % 2 == 1 else None
+            array = [(array[i], array[i + 1]) for i in range(0, len(array), 2)]
+            array = pool.map(merge, array) + ([extra] if extra else [])
 
-    timer.start_for("merge")
+        timer.stop_for("merge")
+        timer.stop_for("total")
 
-    # For a core count greater than 2, we can use multiprocessing
-    # again to merge sub-lists in parallel.
-    while len(results) > 1:
-        with process_pool(size=ps_count) as pool:
-            pool.apply_async(merge_multiple, (results, results.pop(0), results.pop(0)))
-
-    timer.stop_for("merge")
-    timer.stop_for("total")
-
-    final_sorted_list = results[0]
+    final_sorted_list = array[0]
 
     return timer, final_sorted_list
 
 
 def get_command_line_parameters():
-    """Get the process count from command line parameters."""
-    if len(sys.argv) > 1:
-        # Check if the desired number of concurrent processes
-        # has been given as a command-line parameter.
-        total_processes = int(sys.argv[1])
-        if total_processes > 1:
-            # Restrict process count to even numbers
-            if total_processes % 2 != 0:
-                print("Process count should be an even number.")
-                sys.exit(1)
-        print(f"Using {total_processes} cores")
-    else:
-        total_processes = 1
+    """Get the process count, array length from command line parameters."""
 
-    return {"process_count": total_processes}
+    parser = argparse.ArgumentParser(
+        description="""Implement merge sort and multithreaded merge sort.
+        Compare the time required by both algorithms.
+        Also, analyze the performance of each algorithm for the best case and the worst case."""
+    )
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        help="Number of processes to launch",
+        required=False,
+        default=CPU_COUNT,
+        type=lambda x: int(x)
+        if 0 < int(x) <= CPU_COUNT
+        else parser.error(f"Number of processes must be between 1 and {CPU_COUNT}"),
+    )
+    parser.add_argument(
+        "-l",
+        "--length",
+        help="Length of the array to sort",
+        required=False,
+        default=random.randint(3 * 10**6, 4 * 10**6),  # Randomize the length of our list
+        type=lambda x: int(x) if 0 < int(x) else parser.error("Length of the array must be greater than 0"),
+    )
+    parser.add_argument(
+        "-a",
+        "--all",
+        help="Test all the variable length",
+        required=False,
+        default=False,
+        action="store_true",
+    )
+    return parser.parse_args()
 
 
-if __name__ == "__main__":
-    parameters = get_command_line_parameters()
-
-    process_count = parameters["process_count"]
+def main(jobs, length, conclusion):
+    """Main function."""
 
     main_timer = Timer("single_core", "list_generation")
     main_timer.start_for("list_generation")
-
-    # Randomize the length of our list
-    length = random.randint(3 * 10**6, 4 * 10**6)
 
     # Create an unsorted list with random numbers
     randomized_array = [random.randint(0, i * 100) for i in range(length)]
     main_timer.stop_for("list_generation")
 
     print(f"List length: {length}")
-    print(f"Random list generated in {main_timer['list_generation']:.6f}")
-
-    # Start timing the single-core procedure
-    main_timer.start_for("single_core")
-    single = merge_sort(randomized_array)
-    main_timer.stop_for("single_core")
+    print(f"Random list generated in {main_timer['list_generation']:.6f}s\n")
 
     # Create a copy first due to mutation
     randomized_array_sorted = randomized_array[:]
     randomized_array_sorted.sort()
 
+    # Start timing the single-core procedure
+    print("Starting simple sort.")
+    main_timer.start_for("single_core")
+    sorted_array = merge_sort(randomized_array)
+    main_timer.stop_for("single_core")
+
     # Comparison with Python list sort method
     # serves also as validation of our implementation.
-    print("Verification of sorting algorithm:", randomized_array_sorted == single)
-    print(f"Single Core elapsed time: {main_timer['single_core']:.6f} sec")
+    assert sorted_array == randomized_array_sorted, "The sorted array is not correct."
+    print(f"Single Core elapsed time: {main_timer['single_core']:.6f}s\n")
 
     print("Starting parallel sort.")
+    parallel_timer, parallel_sorted_array = parallel_merge_sort(randomized_array, jobs)
+    print(f"Final merge duration: {parallel_timer['merge']:.6f}s")
 
-    parallel_timer, parallel_sorted_list = parallel_merge_sort(
-        randomized_array, process_count
-    )
+    assert parallel_sorted_array == randomized_array_sorted, "The sorted array is not correct."
+    print(f"{jobs}-Core elapsed time: {parallel_timer['total']:.6f}s\n" + "-" * 40, "\n")
 
-    print(f"Final merge duration: {parallel_timer['merge']:.6f} sec")
-    print("Sorted arrays equal:", parallel_sorted_list == randomized_array_sorted)
-    print(f"{process_count}-Core elapsed time: {parallel_timer['total']:.6f} sec")
+    conclusion.append([length, main_timer["single_core"], parallel_timer["total"]])
+
+
+if __name__ == "__main__":
+    parameters = get_command_line_parameters()
+
+    jobs = parameters.jobs
+    length = parameters.length
+    all_cases = parameters.all
+    conclusion = []
+    print(f"Using {jobs} cores\n")
+
+    if all_cases:
+        l = 1
+        while l < 10**8:
+            main(jobs, l, conclusion)
+            l *= 10
+    else:
+        main(jobs, length, conclusion)
+
+    print(tabulate(conclusion, headers=["Array Length", "Single-Threaded", "Multi-Threaded"], tablefmt="outline"))
 
 """
-OUTPUT:
+OUTPUT:> python3 merge_sort.py -j 16
 
 Using 16 cores
-List length: 3252321
-Random list generated in 1.787607
-Verification of sorting algorithm: True
-Single Core elapsed time: 14.204555 sec
+
+List length: 3332517
+Random list generated in 1.826744s
+
+Starting simple sort.
+Single Core elapsed time: 14.580756s
+
 Starting parallel sort.
-Performing final merge.
-Final merge duration: 4.764258 sec
-Sorted arrays equal: True
-16-Core elapsed time: 6.984671 sec
+Final merge duration: 2.910911s
+16-Core elapsed time: 5.131196s
+----------------------------------------
+
++----------------+-------------------+------------------+
+|   Array Length |   Single-Threaded |   Multi-Threaded |
++================+===================+==================+
+|        3332517 |           14.5808 |           5.1312 |
++----------------+-------------------+------------------+
+"""
+
+"""
+OUTPUT:> python3 merge_sort.py -a
+
+Using 16 cores
+
+List length: 1
+Random list generated in 0.000005s
+
+Starting simple sort.
+Single Core elapsed time: 0.000003s
+
+Starting parallel sort.
+Final merge duration: 0.003702s
+16-Core elapsed time: 0.028538s
+----------------------------------------
+
+List length: 10
+Random list generated in 0.000035s
+
+Starting simple sort.
+Single Core elapsed time: 0.000023s
+
+Starting parallel sort.
+Final merge duration: 0.002112s
+16-Core elapsed time: 0.024624s
+----------------------------------------
+
+List length: 100
+Random list generated in 0.000125s
+
+Starting simple sort.
+Single Core elapsed time: 0.000198s
+
+Starting parallel sort.
+Final merge duration: 0.001935s
+16-Core elapsed time: 0.024828s
+----------------------------------------
+
+List length: 1000
+Random list generated in 0.000616s
+
+Starting simple sort.
+Single Core elapsed time: 0.002207s
+
+Starting parallel sort.
+Final merge duration: 0.004501s
+16-Core elapsed time: 0.042195s
+----------------------------------------
+
+List length: 10000
+Random list generated in 0.006530s
+
+Starting simple sort.
+Single Core elapsed time: 0.024342s
+
+Starting parallel sort.
+Final merge duration: 0.009236s
+16-Core elapsed time: 0.035944s
+----------------------------------------
+
+List length: 100000
+Random list generated in 0.061283s
+
+Starting simple sort.
+Single Core elapsed time: 0.297823s
+
+Starting parallel sort.
+Final merge duration: 0.081855s
+16-Core elapsed time: 0.166326s
+----------------------------------------
+
+List length: 1000000
+Random list generated in 0.612833s
+
+Starting simple sort.
+Single Core elapsed time: 3.685065s
+
+Starting parallel sort.
+Final merge duration: 0.645426s
+16-Core elapsed time: 1.235625s
+----------------------------------------
+
+List length: 10000000
+Random list generated in 5.445338s
+
+Starting simple sort.
+Single Core elapsed time: 45.568661s
+
+Starting parallel sort.
+Final merge duration: 5.418382s
+16-Core elapsed time: 11.996948s
+----------------------------------------
+
++----------------+-------------------+------------------+
+|   Array Length |   Single-Threaded |   Multi-Threaded |
++================+===================+==================+
+|              1 |       2.934e-06   |        0.0285377 |
+|             10 |       2.2908e-05  |        0.0246235 |
+|            100 |       0.000198492 |        0.0248281 |
+|           1000 |       0.00220744  |        0.0421945 |
+|          10000 |       0.0243417   |        0.0359442 |
+|         100000 |       0.297823    |        0.166326  |
+|        1000000 |       3.68507     |        1.23563   |
+|       10000000 |      45.5687      |       11.9969    |
++----------------+-------------------+------------------+
 """
